07a56777e4852bec51dae1fd30e1b5e1c3c0a579
amina-min <aminaemcbd@gmail.com>
2022-06-19 15:19:31 +0600

    change app name

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:flutterapp/pages/dashboard.dart';
 import 'package:flutterapp/pages/home_page.dart';
 import 'package:flutterapp/pages/loading_page.dart';
 
-
 void main() {
-
   runApp(MaterialApp(
+    title: 'Celloscope',
     debugShowCheckedModeBanner: false,
     initialRoute: '/',
     routes: {
       '/': (context) => Loading(),
       '/home': (context) => Home(),
-
     },
   ));
 }
